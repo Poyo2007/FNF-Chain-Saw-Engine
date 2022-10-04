2e05fb67a2c39144697941e9e18769b8a72b00a8
Sirox <71220271+Sirox228@users.noreply.github.com>
2022-10-04 20:44:56 +0300

    Create Includes.hx

---
+++ source/macros/Includes.hx
@@ -0,0 +1,31 @@
+package macros;
+
+#if macro
+import haxe.macro.Compiler;
+
+class Includes {
+    public static macro function run():haxe.macro.Expr {
+        #if !display
+        Compiler.include('flixel');
+        #if !web
+        Compiler.include('sys');
+        #end
+        Compiler.include('haxe.crypto');
+        Compiler.include('spinehaxe');
+        #if android
+        Compiler.include('android');
+        #end
+        Compiler.include('haxe.crypto');
+        Compiler.include('DateTools');
+        Compiler.include('Reflect');
+        Compiler.include('EReg');
+        Compiler.include('Lambda');
+        Compiler.include('StringBuf');
+        Compiler.include('StringTools');
+        return macro $v{null};
+        #else
+        return macro $v{null};
+        #end
+    }
+}
+#end
