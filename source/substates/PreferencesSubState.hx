c51caa1e76e299fbaa6776bd26b93bca89b15bf0
Randomxdp <95031664+Randomxdp@users.noreply.github.com>
2022-10-09 21:14:52 -0300

    some descriptions

--- source/substates/PreferencesSubState.hx
+++ source/substates/PreferencesSubState.hx
@@ -9,12 +9,12 @@ class PreferencesSubState extends BaseOptionsSubState
 	{
 		discordClientTitle = 'Preferences Menu';
 
-		addOption(new Option('Ghost Tapping', '---', 'ghostTapping', 'bool', true));
-		addOption(new Option('Downscroll', '---', 'downScroll', 'bool', false));
-		addOption(new Option('Centered Note-Field', '---', 'centeredNotes', 'bool', false));
-		addOption(new Option('Note Splashes', '---', 'noteSplashes', 'bool', true));
+		addOption(new Option('Ghost Tapping', 'If disabled, hitting when theres no note to press will give a miss.', 'ghostTapping', 'bool', true));
+		addOption(new Option('Downscroll', 'If enabled, moves the scroll downwards.', 'downScroll', 'bool', false));
+		addOption(new Option('Centered Note-Field', 'if enabled, moves the scroll to the middle.', 'centeredNotes', 'bool', false));
+		addOption(new Option('Note Splashes', 'If disabled, no splashes will appear on sick note presses.', 'noteSplashes', 'bool', true));
 
-		var option:Option = new Option('Overlay', '---', 'overlay', 'bool', false);
+		var option:Option = new Option('Overlay', 'If enabled, shows an overlay with fps and ram info.', 'overlay', 'bool', false);
 		option.onChange = function()
 		{
 			if (Main.overlay != null)
@@ -23,7 +23,7 @@ class PreferencesSubState extends BaseOptionsSubState
 		addOption(option);
 
 		#if !html5
-		var option:Option = new Option('Framerate', "---", 'framerate', 'int', 60);
+		var option:Option = new Option('Framerate', "The framerate the game runs at.", 'framerate', 'int', 60);
 		option.minValue = 60;
 		option.maxValue = 240;
 		option.onChange = function()
@@ -44,14 +44,14 @@ class PreferencesSubState extends BaseOptionsSubState
 		addOption(option);
 		#end
 
-		var option:Option = new Option('Safe Frames', '---', 'safeFrames', 'int', 10);
+		var option:Option = new Option('Safe Frames', 'The frames you have to press a note.', 'safeFrames', 'int', 10);
 		option.minValue = 2;
 		option.maxValue = 10;
 		addOption(option);
 
-		addOption(new Option('Check For Updates', '---', 'checkForUpdates', 'bool', true));
-		addOption(new Option('Antialiasing', '---', 'antialiasing', 'bool', true));
-		addOption(new Option('Flashing', '---', 'flashing', 'bool', true));
+		addOption(new Option('Check For Updates', 'If disabled, stops checking for updates.', 'checkForUpdates', 'bool', true));
+		addOption(new Option('Antialiasing', 'If disabled, disables antialiasing, increases perfomance \nat the cost of graphics quality.', 'antialiasing', 'bool', true));
+		addOption(new Option('Flashing', 'If disabled, disables all flashing from the engine.', 'flashing', 'bool', true));
 
 		super();
 	}
