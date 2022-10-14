3346584af848a0b0718aa00b2c7bc647c525a7d0
jigsaw-4277821 <mihaialexandru427@yahoo.com>
2022-10-14 23:23:37 +0300

    Revert "Revert "spine update xd""
    This reverts commit 9a6f98d9c8f948bcfce7fac83eec5044bacadfb9.

--- source/macros/Includes.hx
+++ source/macros/Includes.hx
@@ -8,19 +8,18 @@ class Includes
 	public static macro function run():Expr
 	{
 		#if !display
-		Compiler.include('flixel');
-		#if sys
-		Compiler.include('sys');
-		#end
-		Compiler.include('haxe.crypto');
-		Compiler.include('spinehaxe');
 		#if android
 		Compiler.include('android');
 		#end
-		Compiler.include('haxe.crypto');
 		#if cpp
 		Compiler.include('cpp.RawPointer');
 		Compiler.include('cpp.Pointer');
+		#end
+		Compiler.include('haxe.crypto');
+		Compiler.include('flixel');
+		Compiler.include('spine');
+		#if sys
+		Compiler.include('sys');
 		#end
 		#end
 		return macro $v{null};
